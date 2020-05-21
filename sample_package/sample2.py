1848189651ceabc8dc72a5c2c4adc39d40e55274
Fizban <jackmckew2@gmail.com>
2020-05-21 23:26:30 +1000

    Include path

---
+++ sample_package/sample2.py
@@ -0,0 +1,23 @@
+def test_function2(a,b,c):
+    """
+    [summary]
+
+    [extended_summary]
+
+    Args:
+        a ([type]): [description]
+        b ([type]): [description]
+        c ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    return a + b - c
+
+var_1 = 15
+
+var_2 = 20
+
+var_3 = 5
+
+print(test_function2(var_1,var_2,var_3))
